5f5a8663166c3c6ed64f966f6d980838d5953217
mantik0Ra <kirya_prahov@mail.ru>
2022-11-04 15:09:44 +0400

    feat: add solution for problem 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,38 @@
+const fs = require("fs");
+const path = require("path");
+const newFile = path.join(__dirname, "file-copy");
+const oldFile = path.join(__dirname, "files");
+let pathFiles = path.join(__dirname);
+removeFiles("/file-copy");
+addFiles();
+
+function removeFiles(way) {
+    pathFiles += way;
+    fs.readdir(newFile, {withFileTypes: true}, (err, files) => {
+        if(err) {
+            return;
+        }
+        files.forEach(file => {
+            fs.unlink(`${pathFiles}/${file.name}`, err => {
+                if(err) throw err
+            })
+        })
+        
+    })
+}
+
+function addFiles() {
+    fs.mkdir(newFile, { recursive: true }, err => {
+        if(err) throw err;
+    })
+    fs.readdir(oldFile, {withFileTypes: true}, (err, files) => {
+        if(err) throw err;
+        files.forEach(file => {
+            fs.copyFile(`04-copy-directory/files/${file.name}`, `04-copy-directory/file-copy/${file.name}`, err => {
+                if(err) throw err;
+            })
+        })
+    })
+}
+
 
